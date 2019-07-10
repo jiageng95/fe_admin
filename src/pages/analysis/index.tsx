230ae836982498696f8e991fe7974b20cb1ceda3
彭嘉庚 <chris@pengjiagengdeMacBook-Pro.local>
2019-07-11 00:43:29 +0800

    add bizcharts

--- src/pages/analysis/index.tsx
+++ src/pages/analysis/index.tsx
@@ -1,10 +1,31 @@
 import React from 'react'
 import { Card, Row, Col, Icon } from 'antd'
+import { Chart, Geom, Axis, Tooltip, Legend, Coord, Label } from 'bizcharts'
 import ChartCard from '../../components/ChartCard'
 import '../../assets/css/pages/analysis.less'
 
 class Analysis extends React.Component {
     render () {
+        // 数据源
+        const data = [
+            { genre: '1月', sold: 275 },
+            { genre: '2月', sold: 115 },
+            { genre: '3月', sold: 120 },
+            { genre: '4月', sold: 350 },
+            { genre: '5月', sold: 150 },
+            { genre: '6月', sold: 120 },
+            { genre: '7月', sold: 450 },
+            { genre: '8月', sold: 50 },
+            { genre: '9月', sold: 470 },
+            { genre: '10月', sold: 750 },
+            { genre: '11月', sold: 570 },
+            { genre: '12月', sold: 230 }
+        ]
+        // 定义度量
+        const cols = {
+            sold: { alias: '销售量' },
+            genre: { alias: '游戏种类' }
+        }
         return (
             <div className="analysis container">
                 <Row gutter={16} className="mb16">
@@ -96,7 +117,13 @@ class Analysis extends React.Component {
                 <Row className="mb16">
                     <Col span={24}>
                         <Card title="访问量" className="visit_card">
-                            1
+                            <Chart height={320} forceFit={true} data={data} scale={cols}>
+                                <Axis name="genre"/>
+                                <Axis name="sold"/>
+                                {/* <Legend position="top"/> */}
+                                <Tooltip />
+                                <Geom type="interval" position="genre*sold" />
+                            </Chart>
                         </Card>
                     </Col>
                 </Row>
